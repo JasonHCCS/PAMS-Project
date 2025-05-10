84e5bd55418e89afa3e19a39328658b23f62b89f
JasonHCCS <W216263538@student.hccs.edu>
2025-05-09 22:07:12 -0500

    Add files via upload

--- src/main/java/AppointmentBST.java
+++ src/main/java/AppointmentBST.java
@@ -1,5 +1,11 @@
+import java.text.DateFormat;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class AppointmentBST {
+
+    DateFormat dateFormat = new SimpleDateFormat("hh:mm");
+
     private AppointmentNode root;
 
     public AppointmentBST() {
@@ -12,6 +18,28 @@ public class AppointmentBST {
 
     private AppointmentNode insertRec(AppointmentNode root, PatientAppointment appointment) {
         // TODO: Implement logic to insert by appointment time (avoid duplicates)
+        if(root == null) {
+            return new AppointmentNode(appointment);
+        }
+
+        try{
+            Date rootDate = dateFormat.parse(root.data.appointmentTime);
+            Date newDate = dateFormat.parse(appointment.appointmentTime);
+
+            if(rootDate == newDate){
+                return root;
+            }
+            if (newDate.compareTo(rootDate) < 0){
+                root.left = insertRec(root.left, appointment);
+            }
+            else {
+                root.right = insertRec(root.right, appointment);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         return root;
     }
 
